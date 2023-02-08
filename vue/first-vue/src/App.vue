da914b27d7fd68d7264bf4e5b049f7a95ff0ffc7
yjh0816 <2543kt@naver.com>
2023-02-08 16:48:07 +0900

    feat: Update lecture 8

---
+++ vue/first-vue/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>JH</h1>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/first-vue/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>나니아 구역</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
