ec722241a4a7aa64ddd99db783d6046b14477fa4
yjh0816 <2543kt@naver.com>
2023-02-09 14:51:35 +0900

    4. 삭제기능

--- day4/my-todo1/src/components/TodoMain.vue
+++ day4/my-todo1/src/components/TodoMain.vue
@@ -3,13 +3,21 @@
     <header><h1>Vue Fire todo1</h1></header>
     <main>
       <div class="todos">
-        <div class="write">
+        <div class="write" v-if="writeState === 'add'"> <!-- 등록 -->
           <input 
             type="text" 
             v-model="addItemText" 
             @keyup.enter="addItem"
             ref="writeArea"/>
           <button class="btn add" @click.prevent="addItem">Add</button>
+        </div>
+        <div class="write" v-else><!-- 수정 -->
+          <input 
+            type="text" 
+            v-model="editItemText" 
+            @keyup.enter="editSave"
+            ref="writeArea"/>
+          <button class="btn add" @click.prevent="editSave">Save</button>
         </div>
         <ul class="list" v-for="todo,i in todos" :key="i">
           <li>
@@ -19,8 +27,8 @@
             <span>
               {{ todo.text }}
               <b>
-                <a href="">Edit</a>
-                <a href="">Del</a>
+                <a href="" @click.prevent="editShow(i)">Edit</a>
+                <a href="" @click.prevent="delItem(i)">Del</a>
               </b>
             </span>
           </li>
@@ -34,7 +42,10 @@
 export default {
   data() {
     return {
+      writeState: 'add',
       addItemText: '',
+      editItemText: '',
+      crrEditText: '',
       todos:[
         {text: '공부하기', state: 'yet'},
         {text: '운동하기', state: 'done'},
@@ -55,6 +66,22 @@ export default {
     },
     checkItem(idx) {
       this.todos[idx].state = this.todos[idx].state === 'yet' ? 'done' : 'yet';
+    },
+    editShow(idx) {
+      this.crrEditText = idx,
+      this.writeState = 'edit';
+      this.editItemText = this.todos[idx].text;
+    },
+    editSave() {
+      if (!this.editItemText) {
+        return;
+      }
+      this.todos[this.crrEditText].text = this.editItemText;
+      this.editItemText = ''
+      this.writeState = 'add';
+    },
+    delItem(idx) {
+      this.todos.splice(idx, 1);
     },
   },
   mounted() {
