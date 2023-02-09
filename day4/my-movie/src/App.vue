c8ae218006280ee24aa58739232e90f978fb8728
yjh0816 <2543kt@naver.com>
2023-02-09 10:46:44 +0900

    feat: Separate movie component

--- day4/my-movie/src/App.vue
+++ day4/my-movie/src/App.vue
@@ -7,43 +7,18 @@
       <li><a href="">주말관객순</a></li>
       <li><a href="">다운로드순</a></li>
     </ul>
-    <ol class="list" v-for="movie, index in movies" :key="movie.name">
-      <li>
-        <a href="">
-          <h4 class="hide">{{movie.name}}</h4>
-          <span class="hide rate" :class="'rate'+movie.rate[0]">{{movie.rate[1]}}</span>
-          <strong class="rank">{{index + 1}}</strong>
-          <img class="poster" :src="require(`@/assets/${movie.src}`)" :alt=movie.name />
-          <span v-if="moveContext==='평점순'" class="desc">
-              <i class="star"><b :style="'width:'+movie.score*10+'%'"></b></i>
-              <em class="score">{{movie.score}}</em>
-          </span>
-          <span v-else-if="moveContext==='예매순'" class="desc">
-            <i class="reserv_title">예매율</i>
-            <em class="reserv_num">{{movie.ticketing}}</em>
-          </span>
-          <span v-else-if="moveContext==='개봉일순'" class="desc">
-            <i class="open_title">개봉일</i>
-            <em class="open_num">{{movie.openDate}}</em>
-          </span>
-          <span v-else-if="moveContext==='주말관객순'" class="desc">
-            <i class="week_title">주말관객</i>
-            <em class="week_num">{{movie.week}}</em>
-          </span>
-          <span v-else-if="moveContext==='다운로드순'" class="desc">
-            <i class="sale_title">판매율</i>
-            <em class="sale_num">{{movie.sale}}</em>
-          </span>
-        </a>
-      </li>
-    </ol>
+    <movie-list
+      :movies="movies"
+      :moveContext="moveContext"
+    ></movie-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MovieList from './components/MovieList.vue'
 
 export default {
+  components: { MovieList },
   data() {
     return {  
       moveContext: '예매순', 
@@ -153,9 +128,12 @@ export default {
   },
   mounted() {
     this.setCrrTab(this.moveContext);
-    for (movie of this.movies) {
+    this.movies.forEach(movie => {
       movie.rate = [movie.rate, this.rateMatch[movie.rate]]
-    }
+    })
+    // for (movie of this.movies) {
+    //   movie.rate = [movie.rate, this.rateMatch[movie.rate]]
+    // }
     this.setOrder(this.moveContext);
   },
   methods: {
---
+++ day4/my-movie/src/components/MovieList.vue
@@ -0,0 +1,42 @@
+<template>
+  <ol v-for="movie, index in movies" class="list" :key="movie.name">
+    <li>
+      <a href="">
+        <h4 class="hide">{{movie.name}}</h4>
+        <span class="hide rate" :class="'rate'+movie.rate[0]">{{movie.rate[1]}}</span>
+        <strong class="rank">{{index + 1}}</strong>
+        <img class="poster" :src="require(`@/assets/${movie.src}`)" :alt=movie.name />
+        <span v-if="moveContext==='평점순'" class="desc">
+            <i class="star"><b :style="'width:'+movie.score*10+'%'"></b></i>
+            <em class="score">{{movie.score}}</em>
+        </span>
+        <span v-else-if="moveContext==='예매순'" class="desc">
+          <i class="reserv_title">예매율</i>
+          <em class="reserv_num">{{movie.ticketing}}</em>
+        </span>
+        <span v-else-if="moveContext==='개봉일순'" class="desc">
+          <i class="open_title">개봉일</i>
+          <em class="open_num">{{movie.openDate}}</em>
+        </span>
+        <span v-else-if="moveContext==='주말관객순'" class="desc">
+          <i class="week_title">주말관객</i>
+          <em class="week_num">{{movie.week}}</em>
+        </span>
+        <span v-else-if="moveContext==='다운로드순'" class="desc">
+          <i class="sale_title">판매율</i>
+          <em class="sale_num">{{movie.sale}}</em>
+        </span>
+      </a>
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  props:['movies','moveContext'],
+}
+</script>
+
+<style>
+
+</style>
