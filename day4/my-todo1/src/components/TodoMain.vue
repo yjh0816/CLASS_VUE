fb889e794dc16fe3f543d1ed9df24d4d38ec922c
yjh0816 <2543kt@naver.com>
2023-02-09 11:38:34 +0900

    feat: List todo-list

---
+++ day4/my-todo1/src/components/TodoMain.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="page">
+    <header><h1>Vue Fire todo1</h1></header>
+    <main>
+      <div class="todos">
+        <div class="write">
+          <input type="text"/>
+          <button class="btn add">Add</button>
+        </div>
+        <ul class="list" v-for="todo in todos" :key="todo.text">
+          <li>
+            <i class="fa-check-square" :class="'fa'+ (todo.state==='yet' ? 's':'r')"></i>
+            <span>
+              {{ todo.text }}
+              <b>
+                <a href="">Edit</a>
+                <a href="">Del</a>
+              </b>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos:[
+        {text: '공부하기', state: 'yet'},
+        {text: '운동하기', state: 'done'},
+        {text: '글쓰기', state: 'done'},
+      ]
+    }
+  },
+  created: {
+
+  },
+}
+</script>
+
+<style>
+
+</style>
